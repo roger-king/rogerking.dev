2f4b707ad29a2f0abadc41c28564783f9dfe3a64
Roger King <rogr.king@gmail.com>
2019-12-31 11:11:13 -0500

    Updates

---
+++ precache-manifest.68b9921bf23d51d3234970954958a6fb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af4e2851432ee1f199516dd8f4dd2bf",
+    "url": "/rogerking.dev/index.html"
+  },
+  {
+    "revision": "f1faa78009851efde301",
+    "url": "/rogerking.dev/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ab0cda2d2d8f547aef9",
+    "url": "/rogerking.dev/static/js/0.dd62d0b8.chunk.js"
+  },
+  {
+    "revision": "5eeed5ca086720fc5c50",
+    "url": "/rogerking.dev/static/js/3.98cdaa81.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/rogerking.dev/static/js/3.98cdaa81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2c3bec1f4624dc3aab9",
+    "url": "/rogerking.dev/static/js/4.6ff2cb17.chunk.js"
+  },
+  {
+    "revision": "877b6a684a6ab8b24859",
+    "url": "/rogerking.dev/static/js/5.ac76bf15.chunk.js"
+  },
+  {
+    "revision": "180d2184664109d6625b",
+    "url": "/rogerking.dev/static/js/6.4ca0bcab.chunk.js"
+  },
+  {
+    "revision": "3c4e925fb12d62ff50df",
+    "url": "/rogerking.dev/static/js/7.c7d08816.chunk.js"
+  },
+  {
+    "revision": "f1faa78009851efde301",
+    "url": "/rogerking.dev/static/js/main.2a10aebe.chunk.js"
+  },
+  {
+    "revision": "e9cb348e0299cbf27fb1",
+    "url": "/rogerking.dev/static/js/runtime-main.914a2723.js"
+  }
+]);
