913a50ce08e36f6a4777494ed817a56fca79705d
roger-king <rogr.king@gmail.com>
2019-12-31 23:17:28 +0000

    Deploying to gh-pages from  4213f13330bef07f66d73c1b40211c86f78ba194

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rogerking.dev/precache-manifest.df0247e019c775d6e64cf100187c4339.js"
+  "/precache-manifest.d61f8dd3db802d824b349d18e6efb440.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rogerking.dev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
