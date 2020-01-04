5397c33a758d07bd90c427272df1160588fa1279
Roger King <rogr.king@gmail.com>
2020-01-04 14:45:36 -0500

    fix: react-router 404 error

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -17,7 +17,7 @@ const App: React.FC = (): JSX.Element => {
     return (
         <Grommet theme={theme} full>
             <Banner message="Under development" />
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <Header />
                 <Main pad={{ left: '150px', right: '150px' }} overflow="auto" fill>
                     <GlobalContext.Provider
