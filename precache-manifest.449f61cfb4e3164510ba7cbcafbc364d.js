bb139da482dcffab044a293505df5b8eed92a214
roger-king <rogr.king@gmail.com>
2020-01-05 16:06:39 +0000

    Deploying to gh-pages from  28f61e861f9a910df0dc2e0acad68b6ab12a69ce

---
+++ precache-manifest.449f61cfb4e3164510ba7cbcafbc364d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b775f27011040f10a9e2e7114536011e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0e8129e2e6f28bc2dd1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d711200189dacda8af3",
+    "url": "/static/js/0.51921507.chunk.js"
+  },
+  {
+    "revision": "1b2afb3ca8c5192110a1",
+    "url": "/static/js/3.46ebd3d2.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/3.46ebd3d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96043014e1f7a33a6c07",
+    "url": "/static/js/4.58e2085c.chunk.js"
+  },
+  {
+    "revision": "8ed9966da2e50991aa27",
+    "url": "/static/js/5.fa33c328.chunk.js"
+  },
+  {
+    "revision": "0784f6410481fdce7786",
+    "url": "/static/js/6.84c14819.chunk.js"
+  },
+  {
+    "revision": "ea682cf62780013dafb5",
+    "url": "/static/js/7.e2f05ecd.chunk.js"
+  },
+  {
+    "revision": "a0e8129e2e6f28bc2dd1",
+    "url": "/static/js/main.8227b242.chunk.js"
+  },
+  {
+    "revision": "aecef496cb61a60a3555",
+    "url": "/static/js/runtime-main.e932a748.js"
+  }
+]);
