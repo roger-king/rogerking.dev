ac44f6c39b3f120049bef7b8d34eca2554914fc0
Roger King <rogr.king@gmail.com>
2020-01-04 21:51:21 -0500

    fix: padding on mobile

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 import React, { useState, Suspense } from 'react';
-import { Grommet, Main } from 'grommet';
+import { Grommet, Main, ResponsiveContext } from 'grommet';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { theme, workHistory } from './app.constants';
@@ -12,6 +12,9 @@ import Header from './components/header';
 import Footer from './components/footer';
 
 const App: React.FC = (): JSX.Element => {
+    const size = React.useContext(ResponsiveContext);
+    const isMobile = size === 'small';
+    const padding = isMobile ? { left: '15px', right: '15px' } : { left: '150px', right: '150px' };
     const [selectedWork, setSelectedWork] = useState<Work>(workHistory[0]);
 
     return (
@@ -19,7 +22,7 @@ const App: React.FC = (): JSX.Element => {
             <Banner message="Under development" />
             <Router basename="/">
                 <Header />
-                <Main pad={{ left: '150px', right: '150px' }} overflow="auto" fill>
+                <Main pad={padding} overflow="auto" fill>
                     <GlobalContext.Provider
                         value={{
                             selectedWork,
